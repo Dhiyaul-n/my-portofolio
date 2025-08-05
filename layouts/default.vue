6536ec31d1a24d2943098582b6f2e50c7e2429c9
Dhiyaul-n <140733237+Dhiyaul-n@users.noreply.github.com>
2025-08-05 21:17:13 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>
-        <Header/>
-        <main>
-            <slot />
-        </main>
-        <Footer/>
-    </div>
+  <div>
+    <Header />
+    <main>
+      <transition name="fade" mode="out-in">
+        <slot />
+      </transition>
+    </main>
+    <Footer />
+  </div>
 </template>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
